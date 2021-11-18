e8a7defa9467a8e1d467c0a564e88a02e79bcd2a
Harshit Narendrabhai Panchal <84966879+Harshit-tech9@users.noreply.github.com>
2021-11-18 17:59:01 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -42,7 +42,8 @@ def upload_file():
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            #file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file.save(os.path.join('./',filename))
             flash('File successfully uploaded')
             return redirect('/')
         else:
